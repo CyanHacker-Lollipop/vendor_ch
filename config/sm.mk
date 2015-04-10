877ad500530a27b63c446068cd1eed3982bd5b35
noname55804 <marlin2201@gmail.com>
2015-04-10 18:07:04 +0200

    Remove TARGET_LIB_VERSION and change lib path to just lib
    TARGET_LIB_VERSION is no longer needed since the graphite libs are
    in lib of the toolchains.
    
    Commit by SaberMod
    
    Change-Id: I1239f0b86e677e147e34b71c36075b7885e85e18
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- config/sm.mk
+++ config/sm.mk
@@ -62,7 +62,7 @@ ifeq ($(strip $(HOST_OS)),linux)
 
   ifeq ($(strip $(TARGET_ARCH)),arm)
 
-    TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_AND)/lib/gcc/arm-linux-androideabi/$(TARGET_LIB_VERSION).x-sabermod
+    TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_AND)/lib
 
     # Path to ROM toolchain
     GCC_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_AND)
@@ -134,7 +134,7 @@ ifeq ($(strip $(HOST_OS)),linux)
 
   ifeq ($(strip $(TARGET_ARCH)),arm64)
 
-    TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_AND)/lib/gcc/aarch64-linux-android/$(TARGET_LIB_VERSION).x-aosp-sabermod
+    TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_AND)/lib
 
     # Path to toolchain
     GCC_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_AND)
